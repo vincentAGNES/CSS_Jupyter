888ab92833962368e527656fa2ff2924aa78ad7e
agnes <vincent.agnes@eurecom.fr>
2019-11-18 02:55:59 +0100

    update to fix issue with ctrl+d

--- jupyter_js.js
+++ jupyter_js.js
@@ -1,6 +1,14 @@
- require(["codemirror/keymap/sublime", "notebook/js/cell"], function(sublime_keymap, cell) {
-     cell.Cell.options_default.cm_config.keyMap = 'sublime';
- });
+require(["codemirror/keymap/sublime", "notebook/js/cell", "base/js/namespace"],function(sublime_keymap, cell, IPython) {
+// setTimeout(function(){ // uncomment line to fake race-condition
+cell.Cell.options_default.cm_config.keyMap = 'sublime';
+var cells = IPython.notebook.get_cells();
+for(var c=0; c< cells.length ; c++){
+cells[c].code_mirror.setOption('keyMap', 'sublime');
+}
 
- alert("hello world from custom.js")
+// }, 1000)// uncomment line to fake race condition
+}
+);
+
+ alert("hello world from custom.js 2222")
 
