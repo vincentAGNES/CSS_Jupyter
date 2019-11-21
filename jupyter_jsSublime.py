65bbda7e57a52af1f370bfcdcda7f404bd7bc90f
agnes <vincent.agnes@eurecom.fr>
2019-11-21 04:39:10 +0100

    Initial commit for base notebook

--- jupyter_jsSublime.py
+++ jupyter_jsSublime.py
@@ -4,7 +4,7 @@ from urllib.request import urlopen
 jsfile = urlopen('https://raw.githubusercontent.com/vincentAGNES/Jupyter-notebook/master/jupyter_jsSublime.js')
 jscontent = jsfile.read().decode('utf-8')
 jsfile.close()
-Javascript(jscontent)
+Javascript(jscontent) # You might need to run this line in another cell
 del jsfile
 
 # DON'T FORGET TO REMOVE ME WHEN DEV IS DONE :)
