5e4288c75f5d494656116cb4cc2cf71d05e51b0b
agnes <vincent.agnes@eurecom.fr>
2019-11-21 02:26:22 +0100

    creation of jupyter_css.py to import my css

---
+++ jupyter_css.py
@@ -0,0 +1,9 @@
+from IPython.core.display import HTML
+from urllib.request import urlopen
+
+# Styling the notebook.
+cssfile = urlopen('https://raw.githubusercontent.com/vincentAGNES/CSS_Jupyter/master/jupyter_css.css')
+csscontent = cssfile.read().decode('utf-8')
+cssfile.close()
+css_code = '<style>'+'\n'+'{}</style>'.format(csscontent)
+HTML(css_code)
--- jupyter_import.py
+++ jupyter_import.py
@@ -5,12 +5,8 @@ import matplotlib.pyplot as plt
 %matplotlib inline
 
 # Styling
-from IPython.core.display import HTML
+from IPython.core.display import HTML,display,Javascript,Markdown,Latex
 from urllib.request import urlopen
 
-# ctrl D like in sublime Text
-from IPython.display import display,Javascript
-jsfile = urlopen('https://raw.githubusercontent.com/vincentAGNES/Jupyter-notebook/master/jupyter_js.js')
-jscontent = jsfile.read().decode('utf-8')
-jsfile.close()
-Javascript(jscontent)
+# Additional import
+
